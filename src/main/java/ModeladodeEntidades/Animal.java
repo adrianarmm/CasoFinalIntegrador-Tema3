0259effb3584a38aa2fca81230ae2871e8683d5d
adrianarmm <areyemor@myuax.com>
2024-03-19 19:37:48 +0100

    creacion main

--- src/main/java/ModeladodeEntidades/Animal.java
+++ src/main/java/ModeladodeEntidades/Animal.java
@@ -1,11 +1,13 @@
 package ModeladodeEntidades;
 
 
-
 public class Animal extends Organismo {
 
     public Animal(int posicionX, int posicionY, double salud, int edad, boolean estadoReproductivo) {
         super(posicionX, posicionY, salud, edad, estadoReproductivo);
     }
 
+    public double getPoblacion() {
+        return 0;
+    }
 }
--- src/main/java/ModeladodeEntidades/Planta.java
+++ src/main/java/ModeladodeEntidades/Planta.java
@@ -2,10 +2,14 @@ package ModeladodeEntidades;
 
 import ModeladodeEntidades.Organismo;
 
-class Planta extends Organismo {
+public class Planta extends Organismo {
 
     public Planta(int posicionX, int posicionY, double salud, int edad, boolean estadoReproductivo) {
         super(posicionX, posicionY, salud, edad, estadoReproductivo);
+    }
+
+    public Object getPoblacion() {
+        return null;
     }
 }
 
