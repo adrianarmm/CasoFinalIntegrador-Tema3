f94a99c8eb7cbefee59c704f79faa8c8ad92a411
adrianarmm <areyemor@myuax.com>
2024-03-19 18:15:25 +0100

    cambios

--- src/main/java/RegistrodeActividades/Simulacion.java
+++ src/main/java/RegistrodeActividades/Simulacion.java
@@ -7,7 +7,34 @@ import java.time.LocalDateTime;
 public class Simulacion {
     private LocalDateTime fechaHoraInicio;
     private LocalDateTime fechaHoraFin;
-    private List<Especies> especiesList;
-    private List<EventosAleatorio> eventosAleatoriosList;
-    private List<ResultadoSimulacion> resultadosSimulacionList;
+    private List<Especies> especies;
+    private List<EventosAleatorio> eventosAleatorios;
+    private List<ResultadoSimulacion> resultadosSimulacion;
+
+    public Simulacion(LocalDateTime fechaHoraInicio, List<Especies> especiesList, List<EventosAleatorio> eventosAleatorios){
+        this.fechaHoraInicio = fechaHoraInicio;
+        this.especies = especiesList;
+        this.eventosAleatorios = eventosAleatorios;
+        this.resultadosSimulacion = new ArrayList<>();
+
+    }
+
+    public LocalDateTime getFechaHoraInicio(){
+        return fechaHoraInicio;
+    }
+
+    public void setFechaHoraInicio(LocalDateTime fechaHoraInicio){
+        this.fechaHoraInicio = fechaHoraInicio;
+    }
+
+    public LocalDateTime getFechaHoraFin(){
+        return fechaHoraFin;
+    }
+
+    public void setFechaHoraFin(LocalDateTime fechaHoraFin){
+        this.fechaHoraFin = fechaHoraFin;
+    }
 }
+
+public List this.resultados = new ArrayList<>();
+
