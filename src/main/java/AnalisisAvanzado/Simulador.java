eb4b792809ed366528e1c176cb979196fc3f12ea
adrianarmm <areyemor@myuax.com>
2024-03-19 19:18:12 +0100

    creo interfaz

--- src/main/java/AnalisisAvanzado/Simulador.java
+++ src/main/java/AnalisisAvanzado/Simulador.java
@@ -1,4 +1,20 @@
 package AnalisisAvanzado;
 
 public class Simulador {
-}
+        private Modelo modelo;
+
+        public Simulador(Modelo modelo) {
+            this.modelo = modelo;
+        }
+
+        public void simular(double tiempoInicial, double tiempoFinal, double poblacionInicial) {
+            double tiempoActual = tiempoInicial;
+            double poblacionActual = poblacionInicial;
+            while (tiempoActual < tiempoFinal) {
+                poblacionActual = modelo.calcularCrecimiento(tiempoActual, poblacionActual);
+                tiempoActual += 1;
+            }
+            System.out.println("Población final: " + poblacionActual);
+        }
+    }
+
