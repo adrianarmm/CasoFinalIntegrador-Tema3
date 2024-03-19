96f406b1054cdc73c2447bb92903f7380c5a811a
adrianarmm <areyemor@myuax.com>
2024-03-19 17:48:41 +0100

    cambios

--- src/main/java/Autenticación/AutenticacionManager.java
+++ src/main/java/Autenticación/AutenticacionManager.java
@@ -1,12 +1,25 @@
 package Autenticación;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AutenticacionManager {
     private List<Usuario> usuarios;
 
-    public AutenticacionManager(List<Usuario> usuarios) {
-        this.usuarios = usuarios;
+    public AutenticacionManager() {
+        this.usuarios = new ArrayList<>();
     }
 
+    public void addUsuario(Usuario usuario){
+        usuarios.add(usuario);
+    }
+
+    public Usuario autenticar(String usuario, String contraseña){
+        for (Usuario u : usuarios){
+            if (u.getUsuario().equals(usuario) && u.autenticar(contraseña)){
+                return u;
+            }
+        }
+        return null;
+    }
 }
