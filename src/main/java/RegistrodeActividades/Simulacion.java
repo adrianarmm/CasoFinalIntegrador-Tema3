80d01f3fccdc12abfc7112ef7cdfac8240690f58
adrianarmm <areyemor@myuax.com>
2024-03-19 18:15:54 +0100

    cambios

--- src/main/java/RegistrodeActividades/Simulacion.java
+++ src/main/java/RegistrodeActividades/Simulacion.java
@@ -1,40 +1,117 @@
-package RegistrodeActividades;
 import SimuladordeDinámicasPoblacionales.ResultadosdelaSimulación.CrecimientoyReproducción.Especies;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.time.LocalDateTime;
+
 public class Simulacion {
     private LocalDateTime fechaHoraInicio;
     private LocalDateTime fechaHoraFin;
     private List<Especies> especies;
-    private List<EventosAleatorio> eventosAleatorios;
-    private List<ResultadoSimulacion> resultadosSimulacion;
+    private List<EventoAleatorio> eventosAleatorios;
+    private List<ResultadoSimulacion> resultados;
 
-    public Simulacion(LocalDateTime fechaHoraInicio, List<Especies> especiesList, List<EventosAleatorio> eventosAleatorios){
+    public Simulacion(LocalDateTime fechaHoraInicio, List<Especie> especies, List<EventoAleatorio> eventosAleatorios) {
         this.fechaHoraInicio = fechaHoraInicio;
-        this.especies = especiesList;
+        this.especies = especies;
         this.eventosAleatorios = eventosAleatorios;
-        this.resultadosSimulacion = new ArrayList<>();
-
+        this.resultados = new ArrayList<>();
     }
 
-    public LocalDateTime getFechaHoraInicio(){
+    public LocalDateTime getFechaHoraInicio() {
         return fechaHoraInicio;
     }
 
-    public void setFechaHoraInicio(LocalDateTime fechaHoraInicio){
+    public void setFechaHoraInicio(LocalDateTime fechaHoraInicio) {
         this.fechaHoraInicio = fechaHoraInicio;
     }
 
-    public LocalDateTime getFechaHoraFin(){
+    public LocalDateTime getFechaHoraFin() {
         return fechaHoraFin;
     }
 
-    public void setFechaHoraFin(LocalDateTime fechaHoraFin){
+    public void setFechaHoraFin(LocalDateTime fechaHoraFin) {
         this.fechaHoraFin = fechaHoraFin;
     }
+    leatorios;
+
+    public List this.result<Eados = new ArrayList<>();
 }
 
-public List this.resultados = new ArrayList<>();
+public LocalDateTime getFechaHoraInicio() {
+    return fechaHoraInicio;
+}
 
+public void setFechaHoraInicio(LocalDateTime fechaHoraInicio) {
+    specie> get        this.fechaHoraInicio = fechaHoraInicio;
+}
+
+public LocalDateTime getFechaHoraFin() {
+    return fechaHoraFinE;
+}
+
+public void setFechaHoraFin(LocalDateTime fechaHoraFin) {
+    this.fechaHoraFin = fechaHoraFin;
+}
+
+public List<Especie> getEspecies() {
+    return especies;
+}
+
+public void setEspecies(List<Especie> especies) {
+    this.especies = especies;
+}species
+
+public List<EventoAleatorio> getEventosAleatorios() {
+    return eventosAleatorios;
+}
+
+public void setEventosAleator() {
+    returnios(List<EventoAleatorio> eventosAleatorios) {
+        this.eventosAleatorios = eventosAleatorios;
+    }
+
+    public List<ResultadoSimulacion> getResultados() {
+        return resultados;
+    }
+
+    public void espec setResultados(Listies;
+}
+
+public void setEspecies(List<Especie> especies) {
+    this.espec<ResultadoSimulacion> resultados)ies = especies;
+}
+
+    public List<EventoAle {
+    this.atorio> getEventresultados = resultados;
+}osAleatorios
+
+public void() {
+    return eventosAleatorios;
+}
+
+public void setEventosAleatorios(List<Evento guardAleatorio> eventosAleatorios) {
+    arSim this.eventosAleatorios = eventosAleatorios;
+    ulacion() }
+
+public List<ResultadoSimulacion> getResult {
+        // Implementa la lógica para guardadosar la simulación en() {
+        return un archivo o base de dat resultados;
+    }
+
+public void setResultados(List<ResultadoSimulos
+                                  acion> resultados) {
+    this.resultados = result }
+
+public void cados;
+    }argarSimulacion() {
+    // Implement
+
+    public void guardarSimulacion() {
+        // Implementa la lógica para guardar la simulación en una la lógica archivo o base de datos
+    }
+
+    public void c para cargarSimargar la simulación desde un archivo o base de datos
+}ulacion()
+} {
+// Implement
