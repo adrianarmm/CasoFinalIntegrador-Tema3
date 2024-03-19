35203a21c764e2e22c0b3c57a0b31d56ad000535
adrianarmm <areyemor@myuax.com>
2024-03-19 19:20:24 +0100

    cambios

---
+++ src/main/java/AnalisisAvanzado/ModeloPersonalizado.java
@@ -0,0 +1,9 @@
+package AnalisisAvanzado;
+
+public class ModeloPersonalizado implements Modelo {
+    @Override
+    public double calcularCrecimiento(double tiempo, double poblacion) {
+        // Implementa tu propio algoritmo de crecimiento aquí
+        return poblacion * (1 + 0.1 * tiempo);
+    }
+}
