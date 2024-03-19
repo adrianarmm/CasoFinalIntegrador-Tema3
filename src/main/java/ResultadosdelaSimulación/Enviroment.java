dafc200ff669ac69de6b4903058bfa11d27e1d44
adrianarmm <areyemor@myuax.com>
2024-03-19 17:24:00 +0100

    cambios

--- src/main/java/ResultadosdelaSimulación/Enviroment.java
+++ src/main/java/ResultadosdelaSimulación/Enviroment.java
@@ -9,6 +9,7 @@ import java.util.Random;
 public class Environment {
 
     public void EstadisiticasEspecies() {
+        Especies[] especiesList = new Especies[0];
         for (Especies especie : especiesList) {
             System.out.println("Especie: " + especie.getNombre());
             System.out.println("Población: " + especie.getPopulation());
@@ -17,3 +18,23 @@ public class Environment {
             System.out.println("---------------------------------");
         }
     }
+
+    public double calcularTasaDeCrecimiento() {
+    double tasaCrecimiento = 0;
+        Especies[] especiesList = new Especies[0];
+        for (Especies especie : especiesList) {
+        tasaCrecimiento += especie.getCrecimientoRate();
+    }
+    return tasaCrecimiento / especiesList.length;
+    }
+
+    public double calcularTasaDeReproduccion() {
+    double tasaReproduccion = 0;
+        Especies[] especiesList = new Especies[0];
+        for (Especies especie : especiesList) {
+        tasaReproduccion += especie.getReproduccionRate();
+        }
+    return tasaReproduccion / especiesList.length;
+}
+}
+
