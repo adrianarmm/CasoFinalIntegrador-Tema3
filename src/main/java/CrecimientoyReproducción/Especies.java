ac034c6b63a3fff761660d28680cf056bc2bedb4
adrianarmm <areyemor@myuax.com>
2024-03-19 12:54:00 +0100

    cambios

--- src/main/java/CrecimientoyReproducción/Especies.java
+++ src/main/java/CrecimientoyReproducción/Especies.java
@@ -4,15 +4,12 @@ public class Especies {
     private String nombre;
     private double crecimientoRate;
     private double reproduccionRate;
-
     private int population;
 
     public Especies(String nombre, String tipo, String habitat, String alimentacion, String reproduccion) {
         this.nombre = nombre;
-        this.crecimientoRate = crecimiento();
-        this.habitat = habitat;
-        this.alimentacion = alimentacion;
-        this.reproduccionRate = reproduccion;
+        this.crecimientoRate = crecimientoRate();
+        this.reproduccionRate = reproduccionRate();
     }
 
     public void crecimiento() {
